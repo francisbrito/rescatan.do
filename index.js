4fb9a030b9f29b0c826f085ef1f076159af574f6
Francis Brito <fr.br94@gmail.com>
2015-01-16 22:17:18 -0400

    Add application entry point.

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * index.js
+ * Application entry point.
+ *
+ * @author Francis Brito <fr.br94@gmail.com>
+ * @license ISC
+ */
+'use strict';
+
+var app = function (req, res) {
+    res.end('It works!');
+};
+
+exports.app = app;
