59b8f15570b6c284e283406f6bcc8e82dd1c4a58
Francis Brito <fr.br94@gmail.com>
2015-02-19 22:21:00 -0400

    Here goes nothing!

--- index.js
+++ index.js
@@ -11,12 +11,22 @@
     Library imports.
  */
 var express = require('express'),
-    maps = require('./lib/rescue-maps');
+    maps = require('./lib/rescue-maps'),
+    morgan = require('morgan');
 
 /*
     Application setup.
  */
 var app = express();
+
+app.use(morgan('dev'));
+app.use(function ajaxErrorHandler(err, req, res, next) {
+    if (req.xhr) {
+        return res.status(500).json(err);
+    }
+
+    next(err);
+});
 
 /*
     Routing.
